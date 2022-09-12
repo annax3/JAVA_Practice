67b29ecf0e20cfbfec8a25c4d7714422cc9b6dad
Prashant Anand <906120anand@gmail.com>
2022-09-12 06:41:16 +0530

    "ThisKeyword"

---
+++ src/Ja111_Sprint2/Day_6/_1ThisKeyword.java
@@ -0,0 +1,25 @@
+package Ja111_Sprint2.Day_6;
+
+public class _1ThisKeyword {
+    //this point to the current object
+    //static is related to a class not to any object
+    int x=10;
+    void fun(){
+        int x=1000;
+        System.out.println(this);//this point to the current object
+        System.out.println("hey"+this.x);//10 object variable
+        System.out.println(x);//1000 local variable
+    }
+
+    public static void main(String[] args) {
+        int x=100;
+        _1ThisKeyword object=new _1ThisKeyword();
+        System.out.println(object);
+        object.fun();
+        System.out.println(object.x);
+        System.out.println(x);
+        //you can not refer to this keyword from the static area
+
+    }
+
+}
