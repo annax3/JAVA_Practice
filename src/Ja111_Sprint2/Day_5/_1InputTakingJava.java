f371dd8eb771daee3225eda8b37b76737f7921a7
Prashant Anand <906120anand@gmail.com>
2022-09-15 00:22:03 +0530

    "Input_Taking"

--- src/Ja111_Sprint2/Day_5/_1InputTakingJava.java
+++ src/Ja111_Sprint2/Day_5/_1InputTakingJava.java
@@ -6,9 +6,9 @@ public class _1InputTakingJava {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Your age");
-        int age = scanner.nextInt();
+        int age = scanner.nextInt();//for integer
         System.out.println("Enter Your name");
-        String name = scanner.next();
+        String name = scanner.next();//for string
         System.out.println(name+" "+age);
         scanner.close();
 
