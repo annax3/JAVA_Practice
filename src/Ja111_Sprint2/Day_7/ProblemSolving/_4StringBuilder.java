d642d99a4062e50deaadf0b3d155a6e18c180243
Prashant Anand <906120anand@gmail.com>
2022-09-18 14:19:37 +0530

    "StringBuilder"

---
+++ src/Ja111_Sprint2/Day_7/ProblemSolving/_4StringBuilder.java
@@ -0,0 +1,17 @@
+package Ja111_Sprint2.Day_7.ProblemSolving;
+
+public class _4StringBuilder {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("Value");
+        //2 ways to convert a string builder to string
+        //
+       String s= builder.toString();
+       //2
+        String sS= new String(builder);
+       String x="sjsjs";
+       //this Converts string to stringbuilder
+       StringBuilder b=new StringBuilder(x);
+
+
+    }
+}
