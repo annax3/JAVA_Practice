4fbd735ced37a496298b422742392369233b96f6
Prashant Anand <906120anand@gmail.com>
2022-09-21 13:43:51 +0530

    "Student"

--- src/Ja111_Sprint2/Day_7/Student.java
+++ src/Ja111_Sprint2/Day_7/Student.java
@@ -6,4 +6,8 @@ public class Student {
     public Student(String name) {
         this.name = name;
     }
+    @Override
+    public  String toString(){
+        return "Hello from"+name;
+    }
 }
