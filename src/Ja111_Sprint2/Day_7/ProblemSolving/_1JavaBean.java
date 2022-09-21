6274c2babf9b040700a187c363fdd30a097c6145
Prashant Anand <906120anand@gmail.com>
2022-09-21 13:42:52 +0530

    "JAVA_Bean"

--- src/Ja111_Sprint2/Day_7/ProblemSolving/_1JavaBean.java
+++ src/Ja111_Sprint2/Day_7/ProblemSolving/_1JavaBean.java
@@ -17,30 +17,3 @@ public class _1JavaBean {
 
     }
 }
-class Movie{
-    private int duration;
-    private  String name;
-
-    public Movie(int duration, String name) {
-        this.duration = duration;
-        this.name = name;
-    }
-
-    Movie(){}
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
