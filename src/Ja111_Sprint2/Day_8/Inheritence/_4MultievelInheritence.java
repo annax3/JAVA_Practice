da02faac5317535a6b9b0592b30e6d3167a48faf
Prashant Anand <906120anand@gmail.com>
2022-09-21 13:42:03 +0530

    "multiple_Inheritance"

--- src/Ja111_Sprint2/Day_8/Inheritence/_4MultievelInheritence.java
+++ src/Ja111_Sprint2/Day_8/Inheritence/_4MultievelInheritence.java
@@ -3,13 +3,27 @@ package Ja111_Sprint2.Day_8.Inheritence;
 public class _4MultievelInheritence {
     public static void main(String[] args) {
         Child child=new Child();
-        child.facialFeatures();
+        //child.facialFeatures();
     }
 }
 class Grandparent{
+    Grandparent(){
+        //super();
+        System.out.println("GP");
+    }
     void facialFeatures(){
         System.out.println("facialFeatures");
     }
 }
-class Parent extends Grandparent{}
-class Child extends Parent{}
+class Parent extends Grandparent{
+    Parent(){
+        // super();
+        System.out.println("parent");
+    }
+}
+class Child extends Parent{
+    Child(){
+        // super();
+        System.out.println("child");
+    }
+}
