d9c3754e854c3deb1048380d082038b5ac0e5c01
Prashant Anand <906120anand@gmail.com>
2022-09-25 09:57:46 +0530

    "Packages"

---
+++ src/JAVA_111/Day_10/PreClass/_3Packages.java
@@ -0,0 +1,8 @@
+package JAVA_111.Day_10.PreClass;
+
+public class _3Packages {
+    //mechanism to encapsulate the classes(related classes)
+    //1. organising the classes
+    //2 preventing naming conflicts
+
+}
