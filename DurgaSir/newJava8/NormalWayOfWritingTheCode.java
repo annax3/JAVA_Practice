a896f21acfb048855d2b55e2df8b2c3a54254bb1
Prashant Anand <906120anand@gmail.com>
2023-04-13 01:46:41 +0530

    01

---
+++ DurgaSir/newJava8/NormalWayOfWritingTheCode.java
@@ -0,0 +1,9 @@
+public class NormalWayOfWritingTheCode {
+    public static int squareIt(int n){
+        return n*n;
+    }
+    public static void main(String[] args) {
+        System.out.println("The square of 4 is : " +squareIt(4));
+        System.out.println("The square of 5 is : " +squareIt(5));
+    }
+}
