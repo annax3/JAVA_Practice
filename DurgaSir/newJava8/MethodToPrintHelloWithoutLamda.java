12a39ca2fa1e70efd2f8c61cc2741eb99cc56a0b
Prashant Anand <906120anand@gmail.com>
2023-04-13 02:12:54 +0530

    Why_Lamda

---
+++ DurgaSir/newJava8/MethodToPrintHelloWithoutLamda.java
@@ -0,0 +1,10 @@
+public class MethodToPrintHelloWithoutLamda {
+    public void m1(){
+        System.out.println("Hello ji..");
+    }
+
+    public static void main(String[] args) {
+        MethodToPrintHelloWithoutLamda method=new MethodToPrintHelloWithoutLamda();
+        method.m1();
+    }
+}
